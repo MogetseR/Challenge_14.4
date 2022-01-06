d64f9aad82345e878ece9e3735156e1521a99a82
MogetseR <93680431+MogetseR@users.noreply.github.com>
2022-01-06 17:58:45 +0200

    first commit

---
+++ Strings.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Strings{
+   public static void main(String [] args){
+      // Ascending
+      String[] numbers = {"1","4","3","2","3","7","5","6","9"};
+      
+      
+      System.out.println("before sorting: " + Arrays.toString(numbers));
+      
+      Arrays.sort(numbers);
+     
+     System.out.println("After sorting: " + Arrays.toString(numbers));
+     
+     
+     }
+     }
+     
+     
